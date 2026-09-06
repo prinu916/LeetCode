255a5702c78edcfcf0e3f58b038ab392ab272af2
Priyanshu Singh <priyanshusingh71163@gmail.com>
2026-09-06 20:45:23 +0530

    Time: 3 ms (44.32%), Space: 42.4 MB (81.03%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int num = left; num <= right; num++) {
+
+            int temp = num;
+            boolean isSelfDividing = true;
+
+            while (temp > 0) {
+                int digit = temp % 10;
+
+                // Digit 0 cannot divide the number
+                if (digit == 0 || num % digit != 0) {
+                    isSelfDividing = false;
+                    break;
+                }
+
+                temp = temp / 10;
+            }
+
+            if (isSelfDividing) {
+                result.add(num);
+            }
+        }
+
+        return result;
+    }
+}
